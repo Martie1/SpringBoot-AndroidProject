3205ebbbac01f443637fea4c2f4c24aceb28835d
Martie <martiinamay@gmail.com>
2024-10-23 10:55:23 +0200

    Created Entities and UserRepository for Java17

---
+++ backend/kamark/src/main/java/com/kamark/kamark/entity/Room.java
@@ -0,0 +1,34 @@
+package com.kamark.kamark.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name="room")
+public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Integer id;
+
+    @Column(unique = true, length = 100, nullable = false)
+    private String name;
+
+    @CreationTimestamp
+    @Column(updatable = false, name = "created_at")
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+    // one room has many ideas
+    @OneToMany(mappedBy = "room", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Idea> ideas; // many ideas
+}
---
+++ backend/kamark/src/main/java/com/kamark/kamark/entity/User.java
@@ -0,0 +1,121 @@
+package com.kamark.kamark.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name="user")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //do not use GenerationType.AUTO
+    @Column(nullable = false)
+    private Integer id;
+
+    @Column(unique = true, length = 255, nullable = false)
+    private String email;
+    @Column(unique = true, length = 100, nullable = false)
+    private String username;
+
+    @Column(nullable = false,length = 30)
+    private String password;
+
+    @Column(length = 30)
+    private String role;
+
+    @Column(nullable = false)
+    private Integer admin;
+
+    @CreationTimestamp
+    @Column(updatable = false, name = "created_at")
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+    //relationships
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Idea> ideas;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public User setEmail(String email1) {
+        this.email = email;
+        return this;
+    }
+    @Override
+    public String getUsername() {
+        return username;
+    }
+    public User setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
+    public User setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public Integer getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(Integer admin) {
+        this.admin = admin;
+    }
+
+    public Set<Idea> getIdeas() {
+        return ideas;
+    }
+
+    public void setIdeas(Set<Idea> ideas) {
+        this.ideas = ideas;
+    }
+
+    // Getters and setters
+}
