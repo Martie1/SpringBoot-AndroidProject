03564d53d9a520da2ca3e741578f4c15eea74cbe
BroZor <kubulba1988@gmail.com>
2024-11-11 12:47:52 +0100

    working profileActivity, logged in user shows up

--- frontend/app/src/main/java/com/example/pwo/network/ApiService.java
+++ frontend/app/src/main/java/com/example/pwo/network/ApiService.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
 import retrofit2.http.Body;
 import retrofit2.http.Path;
@@ -17,13 +18,13 @@ import retrofit2.http.Path;
 //mapping frontend requests for backend routes
 //ApiService will be used by retrofit.
 public interface ApiService {
-    @POST("auth/register")
+    @POST("/auth/register")
     Call<AuthResponse> register(@Body RegisterRequest registerRequest);
 
-    @POST("auth/login")
+    @POST("/auth/login")
     Call<AuthResponse> login(@Body LoginRequest loginRequest);
 
-    @GET("api/rooms")
+    @GET("/api/rooms")
     Call<List<Room>> getRooms();
 
     @GET("/api/posts/room/{id}")
@@ -32,6 +33,6 @@ public interface ApiService {
     @GET("/api/posts/{id}")
     Call<Post> getPost(@Path("id") int id);
 
-    @GET("/api/user/{token}")
-    Call<User> getUser(@Path("token") String token);
+    @GET("/api/user/profile")
+    Call<User> getUser(@Header("Authorization") String token);
 }
